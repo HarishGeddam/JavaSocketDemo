2ef985b4c96b503703771e9dfcec2fa2582c4ed0
HarishGeddam <hgeddam@aum.edu>
2026-02-05 16:47:17 -0600

    Add files via upload

--- DateServer.java
+++ DateServer.java
@@ -8,22 +8,36 @@ public class DateServer
         try
         {
             ServerSocket sock = new ServerSocket(6013);
-            
+            System.out.println("DateServer started");
+
             while (true)
             {
                 Socket client = sock.accept();
 
-                PrintWriter pout = new
-                 PrintWriter(client.getOutputStream(), true);
+                System.out.println("Client connected ");
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(client.getInputStream()));
+
+                PrintWriter pout =
+                        new PrintWriter(client.getOutputStream(), true);
 
                 pout.println(new java.util.Date().toString());
 
+                String message;
+                while ((message = in.readLine()) != null)
+                {
+                    System.out.println("Client says: " + message);
+                }
+
+
                 client.close();
+                System.out.println("Client disconnected");
             }
         }
         catch (IOException ioe)
         {
-            System.err.println(ioe);
+            System.err.println("Server error: " + ioe.getMessage());
         }
     }
 }
